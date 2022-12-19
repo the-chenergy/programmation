824c1ced7a82321e4fe0b24096dc5a082fbe71aa
Qianlang Chen <qianlangchen@gmail.com>
2022-12-18 16:08:58 -0800

    Add live solution to LeetCode weekly contest 324

---
+++ contests/leetcode_324/b.cc
@@ -0,0 +1,368 @@
+#define SYNC_IO 01  // Synchronize standard IO (cin, scanf, cout, printf)
+#define TRACE_COUT 01  // Use stdout instead of stderr for trace and eprintf
+#define NO_MAIN 01  // Do not declare the main function
+#define DEBUG 01  // If 0 or unset, disable trace, eprintf, and debug blocks
+
+#pragma region programmation {
+#pragma region common {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
+#ifndef DEBUG
+#    define DEBUG 0
+#elif DEBUG
+#    define _GLIBCXX_DEBUG 1
+#    define _GLIBCXX_DEBUG_PEDANTIC 1
+#    define _FORTIFY_SOURCE 2
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+template <typename T>
+int len(const T& x) {
+    return int(x.size());
+}
+template <typename T, size_t size>
+int len(const T (&)[size]) {
+    return size;
+}
+
+#define __argc(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, n, ...) n
+#define _argc(...) __argc(__VA_ARGS__, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define _concat(x, y) __CONCAT(x, y)
+
+#define _range4(b, e, d, i) \
+    for (auto i = b; (d) > 0 ? i < (e) : i > (e); i += d)
+#define _range1(n) _range4(0, n, 1, _i)
+#define _range2(n, i) _range4(0, n, 1, i)
+#define _range3(b, e, i) _range4(b, e, 1, i)
+// range([begin=0], end, [step=1], i) -> for i in range(begin, end, step)
+#define range(...) _concat(_range, _argc(__VA_ARGS__))(__VA_ARGS__)
+
+#define _each2(a, x) for (auto& x : a)
+#define _each_(a, ...) for (auto& [__VA_ARGS__] : a)
+#define _argc2(...) __argc(__VA_ARGS__, _, _, _, _, _, _, _, _, _, _, 2, 1, 0)
+// each(A, ...) -> for ... in A
+#define each(...) _concat(_each, _argc2(__VA_ARGS__))(__VA_ARGS__)
+
+// enumerate(A, i, ...) -> for i, (...) in enumerate(A)
+#define enumerate(a, i, ...)         \
+    for (int i = 0; i != -1; i = -1) \
+    each(a, __VA_ARGS__) for (int _##i = 1; _##i; _##i = 0, i++)
+
+int _ADJ4_DIRS[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+#define _adj42(dy, dx) for (auto [dy, dx] : _ADJ4_DIRS)
+#define _adj44(cy, cx, y, x)                                              \
+    _adj42(_dy, _dx) for (int y = cy + _dy, x = cx + _dx, _##y = 1; _##y; \
+                          _##y = 0)
+#define _adj46(h, w, cy, cx, y, x) \
+    _adj42(y, x) for (y += cy, x += cx; ~y && ~x && y != h && x != w; y = -1)
+// adj4([y_lim], [x_lim], cur_y, cur_x, y, x)
+//     -> for each (y, x) adjacent to (cy, cx), with optional boundary checks
+#define adj4(...) _concat(_adj4, _argc(__VA_ARGS__))(__VA_ARGS__)
+
+#define rep(i, b, e) _range3(b, e, i)
+#define trav(x, a) _each2(a, x)
+#define all(x) begin(x), end(x)
+#define sz len
+
+template <typename TT, typename TS>
+bool amax(TT& t, TS s) {
+    return t < s && (t = s, true);
+}
+template <typename TT, typename TS>
+bool amin(TT& t, TS s) {
+    return s < t && (t = s, true);
+}
+
+// Example: partition_point(all(A), selecting(x, len(x) < 2))
+#define selecting(x, ...) [&](auto& x) { return __VA_ARGS__; }
+
+// Example: sort(all(A), comparing(x, len(x)))
+#define comparing(x, ...)                               \
+    [&](auto& _l, auto& _r) {                           \
+        auto _f = [&](auto& x) { return __VA_ARGS__; }; \
+        return _f(_l) < _f(_r);                         \
+    }
+
+// Example: accumulate(all(A), 0LL, adding(x, len(x)))
+#define adding(x, ...) [&](auto _s, auto& x) { return _s + (__VA_ARGS__); }
+
+// Binary search
+// Example: first_where(0, len(A), i, len(A[i]) >= 2)
+#define first_where(b, e, i, ...)                                        \
+    [&](decltype(e) _b, decltype(e) _e, decltype(e) i) {                 \
+        while (_b < _e)                                                  \
+            i = _b + (_e - _b) / 2, (__VA_ARGS__) ? _e = i : _b = i + 1; \
+        return _b;                                                       \
+    }(b, e, 0)
+
+void _init() __attribute__((constructor));
+void _init() {
+#if !SYNC_IO
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif
+}
+
+namespace std {
+template <typename T, size_t size>
+ostream& operator<<(ostream& out, const array<T, size>& x);
+template <typename TFirst, typename TSecond>
+ostream& operator<<(ostream& out, const pair<TFirst, TSecond>& x);
+template <typename... T>
+ostream& operator<<(ostream& out, const tuple<T...>& x);
+
+template <typename TContainer,
+          typename T = enable_if_t<!is_same<TContainer, string>::value,
+                                   typename TContainer::value_type>>
+ostream& operator<<(ostream& out, const TContainer& x) {
+    out << "{";
+    string delim;
+    for (auto& y : x) out << delim << y, delim = ", ";
+    return out << "}";
+}
+template <typename T, size_t size,
+          enable_if_t<!is_same<T, char>::value, int> = 0>
+ostream& operator<<(ostream& out, const T (&x)[size]) {
+    out << "{";
+    string delim;
+    for (auto& y : x) out << delim << y, delim = ", ";
+    return out << "}";
+}
+template <typename T, size_t size>
+ostream& operator<<(ostream& out, const std::array<T, size>& x) {
+    out << "{";
+    string delim;
+    for (auto& y : x) out << delim << y, delim = ", ";
+    return out << "}";
+}
+template <typename TFirst, typename TSecond>
+ostream& operator<<(ostream& out, const pair<TFirst, TSecond>& x) {
+    return out << "{" << x.first << ", " << x.second << "}";
+}
+template <typename... T>
+ostream& operator<<(ostream& out, const tuple<T...>& x) {
+    out << "{";
+    string delim;
+    apply([&](auto&... y) { (..., (out << delim << y, delim = ", ")); }, x);
+    return out << "}";
+}
+}  // namespace std
+
+struct _teed_cout {
+    bool _opened = false;
+    ofstream _file_out;
+    void open(const char* path) { _file_out.open(path), _opened = true; }
+    template <typename T>
+    _teed_cout& operator<<(const T& x) {
+        return cout << x, _opened && (_file_out << x), *this;
+    }
+} _teed_cout;
+ostream& _cout = cout;
+
+#if TRACE_COUT
+#    define _trace_out cout
+#else
+#    define _trace_out cerr
+// Outputs to both console and file. Access original cout via _cout.
+#    define cout _teed_cout
+#endif
+
+template <typename TArg1>
+void _trace(const char* labels, const TArg1& arg1) {
+    if (labels[0] != '"') _trace_out << labels + (labels[0] == ' ') << ": ";
+    _trace_out << arg1 << "\n";
+}
+
+template <typename TArg1, typename... TArgs>
+void _trace(const char* labels, const TArg1& arg1, const TArgs&... args) {
+    if (labels[0] == ' ') labels++;
+    size_t first_label_size = strchr(labels, ',') - labels;
+    if (labels[0] != '"') _trace_out.write(labels, first_label_size) << ": ";
+    _trace_out << arg1 << ", ", _trace(labels + first_label_size + 1, args...);
+}
+
+template <typename T>
+void _view(const T& x) {
+    _trace_out << x;
+}
+
+template <typename T, typename... TRange>
+void _view(const T& x, int b, int e, TRange... ranges) {
+    b < 0 && (b += len(x)), e < 0 && (e += len(x));
+    b = min(max(b, 0), len(x)), e = min(max(e, b), len(x));
+    auto it = begin(x);
+    advance(it, b);
+    _trace_out << "{";
+    for (int i = b; i < e; it++, i++) {
+        if (i != b) _trace_out << ", ";
+        _view(*it, ranges...);
+    }
+    _trace_out << "}";
+}
+
+#if DEBUG
+// printf into cerr
+#    if TRACE_COUT
+#        define eprintf printf
+#    else
+#        define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#    endif
+// trace(x, ...) -> prints "x: {x}, ..." to cerr
+#    define trace(...) (_trace(#__VA_ARGS__, __VA_ARGS__), 0)
+// view(X, b1, e1, ...) prints X[b1:e1, ...] to cerr
+#    define view(...) (_view(__VA_ARGS__), trace(""), 0)
+#    define debug
+#else
+#    define eprintf(...) (void)0
+#    define trace(...) ((void)0, 0)
+#    define view(...) ((void)0, 0)
+#    define debug while (false)
+#endif
+
+int num_cases = 1, case_id;
+
+#if !NO_MAIN
+void solve_suite();
+int main() { solve_suite(); }
+#endif
+#pragma endregion common }
+
+// Additional library code
+/**
+ * Author: chilli, c1729, Simon Lindholm
+ * Date: 2019-03-28
+ * License: CC0
+ * Source: Wikipedia, https://miller-rabin.appspot.com/
+ * Description: Deterministic Miller-Rabin primality test.
+ * Guaranteed to work for numbers up to $7 \cdot 10^{18}$; for larger numbers, use Python and extend A randomly.
+ * Time: 7 times the complexity of $a^b \mod c$.
+ * Status: Stress-tested
+ */
+/**
+ * Author: chilli, Ramchandra Apte, Noam527, Simon Lindholm
+ * Date: 2019-04-24
+ * License: CC0
+ * Source: https://github.com/RamchandraApte/OmniTemplate/blob/master/modulo.hpp
+ * Description: Calculate $a\cdot b\bmod c$ (or $a^b \bmod c$) for $0 \le a, b \le c \le 7.2\cdot 10^{18}$.
+ * Time: O(1) for \texttt{modmul}, O(\log b) for \texttt{modpow}
+ * Status: stress-tested, proven correct
+ * Details:
+ * This runs ~2x faster than the naive (__int128_t)a * b % M.
+ * A proof of correctness is in doc/modmul-proof.tex. An earlier version of the proof,
+ * from when the code used a * b / (long double)M, is in doc/modmul-proof.md.
+ * The proof assumes that long doubles are implemented as x87 80-bit floats; if they
+ * are 64-bit, as on e.g. MSVC, the implementation is only valid for
+ * $0 \le a, b \le c < 2^{52} \approx 4.5 \cdot 10^{15}$.
+ */
+typedef unsigned long long ull;
+ull modmul(ull a, ull b, ull M) {
+	ll ret = a * b - M * ull(1.L / M * a * b);
+	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = modmul(b, b, mod), e /= 2)
+		if (e & 1) ans = modmul(ans, b, mod);
+	return ans;
+}
+
+bool isPrime(ull n) {
+	if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+	ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+	    s = __builtin_ctzll(n-1), d = n >> s;
+	for (ull a : A) {   // ^ count trailing zeroes
+		ull p = modpow(a%n, d, n), i = s;
+		while (p != 1 && p != n - 1 && a % n && i--)
+			p = modmul(p, p, n);
+		if (p != n-1 && i != s) return 0;
+	}
+	return 1;
+}
+
+/**
+ * Author: chilli, SJTU, pajenegod
+ * Date: 2020-03-04
+ * License: CC0
+ * Source: own
+ * Description: Pollard-rho randomized factorization algorithm. Returns prime
+ * factors of a number, in arbitrary order (e.g. 2299 -> \{11, 19, 11\}).
+ * Time: $O(n^{1/4})$, less for numbers with small factors.
+ * Status: stress-tested
+ *
+ * Details: This implementation uses the improvement described here
+ * (https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm#Variants), where
+ * one can accumulate gcd calls by some factor (40 chosen here through
+ * exhaustive testing). This improves performance by approximately 6-10x
+ * depending on the inputs and speed of gcd. Benchmark found here:
+ * (https://ideone.com/nGGD9T)
+ *
+ * GCD can be improved by a factor of 1.75x using Binary GCD
+ * (https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/).
+ * However, with the gcd accumulation the bottleneck moves from the gcd calls
+ * to the modmul. As GCD only constitutes ~12% of runtime, speeding it up
+ * doesn't matter so much.
+ *
+ * This code can probably be sped up by using a faster mod mul - potentially
+ * montgomery reduction on 128 bit integers.
+ * Alternatively, one can use a quadratic sieve for an asymptotic improvement,
+ * which starts being faster in practice around 1e13.
+ *
+ * Brent's cycle finding algorithm was tested, but doesn't reduce modmul calls
+ * significantly.
+ *
+ * Subtle implementation notes:
+ * - we operate on residues in [1, n]; modmul can be proven to work for those
+ * - prd starts off as 2 to handle the case n = 4; it's harmless for other n
+ *   since we're guaranteed that n > 2. (Pollard rho has problems with prime
+ *   powers in general, but all larger ones happen to work.)
+ * - t starts off as 30 to make the first gcd check come earlier, as an
+ *   optimization for small numbers.
+ */
+ull pollard(ull n) {
+	auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+	ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+	while (t++ % 40 || __gcd(prd, n) == 1) {
+		if (x == y) x = ++i, y = f(x);
+		if ((q = modmul(prd, max(x,y) - min(x,y), n))) prd = q;
+		x = f(x), y = f(f(y));
+	}
+	return __gcd(prd, n);
+}
+vector<ull> factor(ull n) {
+	if (n == 1) return {};
+	if (isPrime(n)) return {n};
+	ull x = pollard(n);
+	auto l = factor(x), r = factor(n / x);
+	l.insert(l.end(), all(r));
+	return l;
+}
+
+#pragma GCC diagnostic pop
+#pragma endregion programmation }
+
+
+
+class Solution {
+public:
+    int smallestValue(int ne) {
+        set<ull>s;
+        ull n=ne;
+        while(1){
+            auto f=factor(n);
+            ull x=0;each(f,y)x+=y;
+            if(s.count(x))return *s.begin();
+            s.insert(x);
+            n=x;
+        }
+    }
+};
