f0432d30e857a381501ffef0d91903b3e8d6cd9b
Qianlang Chen <qianlangchen@gmail.com>
2023-02-01 14:51:17 -0800

    Update C++ solution template
    - With shortened helper code

--- templates/solution.cc
+++ templates/solution.cc
@@ -1,190 +1,79 @@
-// #define SYNC_IO 01  // Synchronize cin and cout
-// #define TRACE_COUT 01  // Use stdout for trace and eprintf
+// #define SYNC_IO 01  // Synchronize standard IO (cin, scanf, cout, printf)
+// #define TRACE_COUT 01  // Use stdout instead of stderr for trace and eprintf
 // #define NO_MAIN 01  // Do not declare the main function
-// #define LOCAL 0  // If 0 or not set, "comment" all trace and eprintf
+// #define DEBUG 0  // If 0 or unset, disable trace, eprintf, and debug blocks
 
-#pragma region {
-#if LOCAL
+#pragma region programmation {
+#pragma region common {  // clang-format off
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
+#ifndef DEBUG
+#    define DEBUG 0
+#endif
+#if DEBUG
 #    define _GLIBCXX_DEBUG 1
 #    define _GLIBCXX_DEBUG_PEDANTIC 1
 #    define _FORTIFY_SOURCE 2
+#    pragma GCC optimize("trapv")
+#    if TRACE_COUT
+#        define eprintf printf
+#    else
+#        define eprintf(...) fprintf(stderr, __VA_ARGS__)  // printf into cerr
+#    endif
+#    define trace(...) (_trace(#__VA_ARGS__, __VA_ARGS__), 0)  // trace(x, ...) -> prints "x: {x}, ..." to cerr
+#    define view(...) (_view(__VA_ARGS__), trace(""), 0)  // view(X, b1, e1, ...) prints X[b1:e1, ...] to cerr
+#    define debug
+#else
+#    define eprintf(...) (void)0
+#    define trace(...) ((void)0, 0)
+#    define view(...) ((void)0, 0)
+#    define debug while (0)
 #endif
-
 #include <bits/stdc++.h>
-
-using namespace std;
-
-using uint = unsigned int;
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using vi = vector<int>;
-using pii = pair<int, int>;
-
 #define rep(i, b, e) for (int i = b; i < (e); i++)
 #define trav(x, a) for (auto& x : a)
 #define all(x) begin(x), end(x)
-
-template <typename T>
-int len(const T& x) {
-    return int(x.size());
-}
-
-template <typename T, size_t size>
-int len(const T (&)[size]) {
-    return size;
-}
-
 #define sz len
-
-void _init() __attribute__((constructor));
-void _init() {
-#if !SYNC_IO
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-#endif
-}
-
-namespace std {
-template <typename T, size_t size>
-ostream& operator<<(ostream& out, const array<T, size>& x);
-
-template <typename TFirst, typename TSecond>
-ostream& operator<<(ostream& out, const pair<TFirst, TSecond>& x);
-
-template <typename... T>
-ostream& operator<<(ostream& out, const tuple<T...>& x);
-
-template <typename TContainer,
-          typename T = enable_if_t<!is_same<TContainer, string>::value,
-                                   typename TContainer::value_type>>
-ostream& operator<<(ostream& out, const TContainer& x) {
-    out << "{";
-    string delim;
-    for (auto& y : x) out << delim << y, delim = ", ";
-    return out << "}";
-}
-
-template <typename T, size_t size,
-          enable_if_t<!is_same<T, char>::value, int> = 0>
-ostream& operator<<(ostream& out, const T (&x)[size]) {
-    out << "{";
-    string delim;
-    for (auto& y : x) out << delim << y, delim = ", ";
-    return out << "}";
-}
-
-template <typename T, size_t size>
-ostream& operator<<(ostream& out, const std::array<T, size>& x) {
-    out << "{";
-    string delim;
-    for (auto& y : x) out << delim << y, delim = ", ";
-    return out << "}";
-}
-
-template <typename TFirst, typename TSecond>
-ostream& operator<<(ostream& out, const pair<TFirst, TSecond>& x) {
-    return out << "{" << x.first << ", " << x.second << "}";
-}
-
-template <typename... T>
-ostream& operator<<(ostream& out, const tuple<T...>& x) {
-    out << "{";
-    string delim;
-    apply([&](auto&... y) { (..., (out << delim << y, delim = ", ")); }, x);
-    return out << "}";
-}
-}  // namespace std
-
+#define _print_vector_impl s << "{"; string d; for (auto&& y : x) s << d << y, d = ", "; return s << "}";
+using namespace std; using ll = long long; using ull = unsigned long long; using ld = long double; using vi = vector<int>; using pii = pair<int, int>; template <typename T> int len(const T& x) { return int(x.size()); } template <typename T, size_t l> int len(const T (&)[l]) { return l; } template <typename T, typename S> bool amax(T& t, S s) { return t < s && (t = s, 1); } template <typename T, typename S> bool amin(T& t, S s) { return s < t && (t = s, 1); } int num_cases = 1, case_id; namespace std { template <typename T, size_t l> ostream& operator<<(ostream& s, const array<T, l>& x); template <typename F, typename S> ostream& operator<<(ostream& s, const pair<F, S>& x); template <typename... T> ostream& operator<<(ostream& s, const tuple<T...>& x); template <typename C, typename T = enable_if_t<!is_same<C, string>::value, typename C::value_type>> ostream& operator<<(ostream& s, const C& x) { _print_vector_impl } template <typename T, size_t l, enable_if_t<!is_same<T, char>::value, int> = 0> ostream& operator<<(ostream& s, const T (&x)[l]) { _print_vector_impl } template <typename T, size_t l> ostream& operator<<(ostream& s, const std::array<T, l>& x) { _print_vector_impl } template <typename F, typename S> ostream& operator<<(ostream& s, const pair<F, S>& x) { return s << "{" << x.first << ", " << x.second << "}"; } template <typename... T> ostream& operator<<(ostream& s, const tuple<T...>& x) { s << "{"; string d; apply([&](auto&&... y) { (..., (s << d << y, d = ", ")); }, x); return s << "}"; } } struct _teed_cout { bool _b = 0; ofstream _f; void open(const char* s) { _f.open(s), _b = 1; } template <typename T> _teed_cout& operator<<(const T& x) { return cout << x, _b && (_f << x), *this; } } _teed_cout; ostream& _cout = cout;
 #if TRACE_COUT
 #    define _trace_out cout
 #else
 #    define _trace_out cerr
+#    define cout _teed_cout  // Outputs to both console and file. Access original cout via _cout.
 #endif
-
-template <typename TArg1>
-void _trace(const char* labels, const TArg1& arg1) {
-    if (labels[0] != '"') _trace_out << labels + (labels[0] == ' ') << ": ";
-    _trace_out << arg1 << "\n";
-}
-
-template <typename TArg1, typename... TArgs>
-void _trace(const char* labels, const TArg1& arg1, const TArgs&... args) {
-    if (labels[0] == ' ') labels++;
-    size_t first_label_size = strchr(labels, ',') - labels;
-    if (labels[0] != '"') _trace_out.write(labels, first_label_size) << ": ";
-    _trace_out << arg1 << ", ";
-    _trace(labels + first_label_size + 1, args...);
-}
-
-template <typename T>
-void _view(const T& x) {
-    _trace_out << x;
-}
-
-template <typename T, typename... TRange>
-void _view(const T& x, int b, int e, TRange... ranges) {
-    b = max(0, min(len(x), b)), e = max(b, min(len(x), e));
-    string delim = "";
-    auto it = begin(x);
-    advance(it, b);
-    auto& out = _trace_out;
-    out << "{";
-    for (; b < e; it++, b++) out << delim, _view(*it, ranges...), delim = ", ";
-    out << "}";
-}
-
-#if LOCAL
-#    if TRACE_COUT
-#        define eprintf printf
-#    else
-#        define eprintf(...) fprintf(stderr, __VA_ARGS__)
-#    endif
-#    define trace(...) _trace(#__VA_ARGS__, __VA_ARGS__)
-#    define view(...) _view(__VA_ARGS__), trace("")
-#    define _local
-#else
-// printf into cerr
-#    define eprintf(...) (void)0
-// trace(x, ...) -> prints "x: {x}, ..." to cerr
-#    define trace(...) (void)0
-// view(X, a, b, ...) prints X[a:b, ...] to cerr
-#    define view(...) (void)0
-#    define _local while (false)
+template <typename T> void _trace(const char* l, const T& x) { if (l[0] != '"') _trace_out << l + (l[0] == ' ') << ": "; _trace_out << x << "\n"; } template <typename T, typename... A> void _trace(const char* l, const T& x, const A&... a) { if (l[0] == ' ') l++; size_t s = strchr(l, ',') - l; if (l[0] != '"') _trace_out.write(l, s) << ": "; _trace_out << x << ", ", _trace(l + s + 1, a...); } template <typename T> void _view(const T& x) { _trace_out << x; } template <typename T, typename... R> void _view(const T& x, int b, int e, R... r) { b < 0 && (b += len(x)), e < 0 && (e += len(x)); b = min(max(b, 0), len(x)), e = min(max(e, b), len(x)); auto it = begin(x); advance(it, b); _trace_out << "{"; for (int i = b; i < e; it++, i++) { if (i != b) _trace_out << ", "; _view(*it, r...); } _trace_out << "}"; }
+#if !SYNC_IO
+void _init() __attribute__((constructor)); void _init() { ios::sync_with_stdio(0), cin.tie(0); }
 #endif
-
 #if !NO_MAIN
-void solve_suite();
-int main() { solve_suite(); }
+void solve_suite(); int main() { solve_suite(); }
 #endif
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wshadow"
+#pragma endregion common }  // clang-format on
 
 // Additional library code
 
-
 #pragma GCC diagnostic pop
-
-#pragma endregion }
-
+#pragma endregion programmation }
 
 
-void solve(int /* case_id */) {
+
+void solve() {
+    
+    if (0 || !cin) exit(0);
     
 }
 
 void solve_suite() {
-    // freopen("./sandbox/i.txt", "r", stdin);
-    // freopen("./sandbox/o.txt", "w", stdout);
-
-    int num_cases = 1 << 0;
+    // debug freopen("./sandbox/i.txt", "r", stdin);
+    // debug cout.open("./sandbox/o.txt");
     // cin >> num_cases;
-    for (int case_id = 1; case_id <= num_cases; case_id++) {
+    // num_cases = 1E9;
+    for (case_id = 1; case_id <= num_cases; case_id++) {
+        // debug cerr.clear(case_id ==  ? _S_goodbit : _S_failbit);
         // cout << "Case #" << case_id << ": ";
-        solve(case_id);
-        // cout << "\n";
-
-        // fprintf(stderr, "(%.2LF s)\n", ld(clock()) / CLOCKS_PER_SEC);
+        solve();
+        cout << "\n";
+        // eprintf("(%.2LF s)\n", ld(clock()) / CLOCKS_PER_SEC / case_id);
     }
 }
